b345d0ed9ab2bffd137c6e2246d576a63313104b
ritikkumarmandal <ritikkumarmandal@Ritiks-MacBook-Air.local>
2026-07-05 16:50:35 +0530

    resaul deploy issu

---
+++ server/types/express.d.ts
@@ -0,0 +1,17 @@
+import { Role } from "@prisma/client";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: Role;
+      };
+    }
+  }
+}
+
+declare module "compression";
+
+export {};
