e9cb602afa8262c1139258b21590d8f00bf719e4
ritikkumarmandal <ritikkumarmandal@Ritiks-MacBook-Air.local>
2026-07-05 17:03:38 +0530

    Fix TypeScript declarations

--- server/types/express.d.ts
+++ server/types/express.d.ts
@@ -1,17 +1,13 @@
 import { Role } from "@prisma/client";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        id: string;
-        email: string;
-        role: Role;
-      };
-    }
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: {
+      id: string;
+      email: string;
+      role: Role;
+    };
   }
 }
 
 declare module "compression";
-
-export {};
