ee355701b209298333622e2aa3f3f970b3df202c
ritikkumarmandal <ritikkumarmandal@Ritiks-MacBook-Air.local>
2026-07-07 13:08:48 +0530

    routes fix

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
- baseURL: "http://filemoon-production.up.railway.app/api",
-  //baseURL: "http://localhost:5003/api",
+ baseURL: "https://filemoon-production.up.railway.app/api",
   withCredentials: true,
 });
