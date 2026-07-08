56bbd4f2bfa62ea18f4cd73974ce4ebb290b91a1
ritikkumarmandal <ritikkumarmandal@Ritiks-MacBook-Air.local>
2026-07-08 12:40:53 +0530

    email fix

--- server/services/mail.service.ts
+++ server/services/mail.service.ts
@@ -1,15 +1,14 @@
 import nodemailer from "nodemailer";
-
 class MailService {
-  private transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST || "smtp.gmail.com",
-    port: Number(process.env.MAIL_PORT) || 587,
-    secure: Number(process.env.MAIL_PORT) === 465,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
+private transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
   constructor() {
     this.transporter.verify((error, success) => {
       if (error) {
